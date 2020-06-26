01153407b65e5227cbd4f852d95dae1be1537eeb
kato-zup <leonardo>
2020-06-26 19:43:32 -0300

    feat: implementado form page

---
+++ src/views/FormPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="formulary-wrapper">
+    <Formulary />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Formulary from '@/components/Formulary.vue';
+
+export default {
+  name: 'FormPage',
+  components: {
+    Formulary,
+  },
+};
+</script>
